e495562c9c9865e03a3c1b19354997bca6e2c1f1
Shiva Kumar <shivakumarkothapally5@gmail.com>
2026-06-23 23:47:39 +0530

    "Configured the Jwt secret and added health controller"

--- backend/src/main/java/com/shiva/quickbite/service/JwtService.java
+++ backend/src/main/java/com/shiva/quickbite/service/JwtService.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -14,11 +15,11 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET =
-            "myverysecuresecretkeymyverysecuresecretkey123456";
+    @Value("${jwt.secret}")
+    private String secret;
 
     private final SecretKey secretKey = Keys.hmacShaKeyFor(
-            SECRET.getBytes(StandardCharsets.UTF_8)
+            secret.getBytes(StandardCharsets.UTF_8)
     );
 
     public String generateToken(String email) {
